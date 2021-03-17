9c7f8f9ce2d69bca2f070f9e146ae83a48699b88
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2021-03-16 20:08:47 -0700

    Add MochaJS (#3)

---
+++ test/test.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Example Spec', function () {
+   it('Example Test', function () {
+      expect(true, 'Truthy value found').to.be.true;
+   });
+});
