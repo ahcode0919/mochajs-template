48cae9a9bdfc345a83924b8b531ffe1caa29dae2
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2021-03-20 23:19:32 -0700

    Add Additional Tests

---
+++ test/test-2.spec.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+
+describe('Outer Describe', function () {
+    it('Example Test 1', function () {
+        expect(true, 'Truthy value found').to.be.true;
+    });
+
+    describe('Inner Describe', function () {
+        it('Example Test 2', function () {
+            expect([1, 2, 3], 'Arrays did not match').to.deep.equal([1, 2, 3]);
+        });
+
+        it('Example Test 3', function () {
+            expect(null).to.be.null;
+        });
+    });
+});
--- test/test.spec.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('Example Spec', function () {
-   it('Example Test', function () {
-      expect(true, 'Truthy value found').to.be.true;
-   });
-});
